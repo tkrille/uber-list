5c535e09a564273c0172aaba0d7fe2962ff273cd
André Gernandt <andre.gernandt@googlemail.com>
2013-02-27 13:35:04 +0100

    +angular-strap, -ui-boostrap

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('uber-list', [ 'ng', 'ui', 'ui.bootstrap', 'los4ng', 'uber-list.services', 'uber-list.controllers', 'uber-list.directives' ]);
+angular.module('uber-list', [ 'ng', 'ui', '$strap', 'los4ng', 'uber-list.services', 'uber-list.controllers', 'uber-list.directives' ]);
