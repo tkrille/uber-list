51e763cc62836fa6cb21aaac8acff483706dce42
xqf23 <tkrille@gmailcom>
2013-03-13 20:53:17 +0100

    removed unused

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,12 +1,3 @@
 'use strict';
 
-angular.module('uber-list.directives', []).
-
-directive('typeahead', function($document) {
-	return function(scope, element, attr) {
-		element.context.autocomplete = "off";
-		element.typeahead({
-			source: scope.typeaheadValues
-		});
-	};
-});
+angular.module('uber-list.directives', []);
